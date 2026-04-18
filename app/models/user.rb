1519e3e7202dc156efd158a7108cc2017412478c
Mohamed Hamdy <99550272+MohamedHamada13@users.noreply.github.com>
2026-04-18 19:47:30 +0200

    Update: giving alias name for association rel between user & posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-    has_many :posts
+    # has_many :posts
+    has_many :created_posts, class_name: "Post", foreign_key: "user_id"
+    # Give an alias to the post association, and specify the foreign key to use for the association. 
+    # This allows us to use created_posts instead of posts when accessing the user's posts.  
 end
