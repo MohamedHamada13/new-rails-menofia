bb6e79a302004342c552a4dda202bf887dd9be5d
Mohamed Hamdy <99550272+MohamedHamada13@users.noreply.github.com>
2026-04-18 19:44:39 +0200

    Associate between user & posts

---
+++ db/schema.rb
@@ -0,0 +1,19 @@
+ActiveRecord::Schema[8.1].define(version: 2026_04_18_174148) do
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "posts", "users"
+end
